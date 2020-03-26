f71096f87444e77068d385978f54fdc8ba49b0cd
Tate Keller <tatek1993@gmail.com>
2020-03-25 19:07:41 -0500

    tests for repair and succeed are working

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,26 +6,27 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item,
-    enhancement: (item.enhancement > 20) ? item.enhancement++ : 20
+  return { 
+    ...item,
+    enhancement: (item.enhancement < 20) ? ++item.enhancement : 20
    
   };
 }
 
 function fail(item) {
   
-  if(item.enhancement > 16) {
-    item.enhancement--;
-  };
-  
   if(item.enhancement < 15) {
-    item.durability - 5;
+    item.durability - 5 
   } else {
-      item.durability - 10;
+    item.durability - 10
+  };
+
+  if(item.enhancement > 16) {
+    --item.enhancement;
   };
   
   return item;
-}
+};
 
 function repair(item) {
   return { 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,5 +2,53 @@ const enhancer = require('./enhancer.js');
 // test away!
 
 it('should run the tests', function() {
-    expect(true).toBe(false);
+    expect(true).toBe(true);
 });
+
+describe('enhancer.js', function() {
+
+    describe('.repair()', function() {
+
+        it('should return a new item with a durability of 100', function() {
+            
+            // expect(enhancer.repair({durability: 0}).durability).toBe(90);
+            expect(enhancer.repair({durability: 30}).durability).toBe(100);
+            expect(enhancer.repair({durability: 84}).durability).toBe(100);
+            expect(enhancer.repair({durability: 0}).durability).toBe(100);
+
+        })
+    });
+
+    describe('.succeed()', function() {
+        it('should increase enhancement by 1', function() {
+
+            // expect(enhancer.succeed({enhancement: 5}).enhancement).toBe(11);
+            expect(enhancer.succeed({enhancement: 3}).enhancement).toBe(4);
+            expect(enhancer.succeed({enhancement: 10}).enhancement).toBe(11);
+
+        });
+
+        it('should not change if enhancement is 20', function() {
+            // expect(enhancer.succeed({enhancement: 20}).enhancement).toBe(19);
+            expect(enhancer.succeed({enhancement: 20}).enhancement).toBe(20);
+
+        });
+
+        it('should not make any changes to durability', function() {
+
+            // expect(enhancer.succeed({enhancement: 10, durability: 50}).durability).toBe(51);
+            expect(enhancer.succeed({enhancement: 10, durability: 50}).durability).toBe(50);
+        })
+    });
+
+    describe('.fail()', function() {
+        it('should decrease the durability by five if the enhancement is less than 15', function() {
+
+            // expect(enhancer.fail({enhancement: 10, durability: 50}).durability).toBe(56); 
+
+            expect(enhancer.fail({enhancement: 10, durability: 50}).durability).toBe(45);
+            expect(enhancer.fail({enhancement: 14, durability: 78}).durability).toBe(73);  
+
+        })
+    })
+})
